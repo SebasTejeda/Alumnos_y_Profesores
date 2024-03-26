2e07e739c7b399fc4685e201bd56b333a874ae25
Sebastian Tejeda <tejedasebastian129@gmail.com>
2024-03-26 11:55:30 -0500

    Se terminó el código, solo falta saber si los estudiantes fueron a clases

--- src/main/java/pe/edu/upc/application/Program.java
+++ src/main/java/pe/edu/upc/application/Program.java
@@ -1,23 +1,42 @@
 package main.java.pe.edu.upc.application;
 
+import main.java.pe.edu.upc.entities.Aula;
 import main.java.pe.edu.upc.entities.Estudiante;
 import main.java.pe.edu.upc.entities.Profesor;
 import main.java.pe.edu.upc.entities.enums.Materias;
+import main.java.pe.edu.upc.entities.enums.Sexo;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        Profesor profesor = new Profesor("Jorge Donayre", 45, "M",
+        Profesor profesor = new Profesor("Jorge Donayre", 45, Sexo.M,
                 Materias.MATEMATICAS);
         List<Estudiante> estudiantes = new ArrayList<>();
 
         Estudiante estudiante1 = new Estudiante("Sebastian Tejeda", 22,
-                "M", 18);
-        Estudiante estudiante2 = new Estudiante("Juan Torres", 25, "M",
+                Sexo.M, 18);
+        Estudiante estudiante2 = new Estudiante("Juan Torres", 25, Sexo.M,
                 15);
         Estudiante estudiante3 = new Estudiante("Samantha Bazalar", 23,
-                "F", 19);
+                Sexo.F, 19);
+        Estudiante estudiante4 = new Estudiante("Sofia Carranza", 22, Sexo.F,
+                15);
+
+        estudiantes.add(estudiante1);
+        estudiantes.add(estudiante2);
+        estudiantes.add(estudiante3);
+        estudiantes.add(estudiante4);
+
+        Aula aula = new Aula(1234, 10, Materias.MATEMATICAS,profesor,
+                estudiantes);
+
+        if(aula.seDaraClases()){
+            System.out.println("Sí se dará clases");
+        }
+        else{
+            System.out.println("No se darán clases hoy");
+        }
     }
 }
