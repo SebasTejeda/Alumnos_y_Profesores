3481a6438000239fed834a40fecf323979229184
Sebastian Tejeda <tejedasebastian129@gmail.com>
2024-03-26 16:55:06 -0500

    Se ha actualizado el método seDaraClases y además se creó el método aprobados

--- src/main/java/pe/edu/upc/entities/Aula.java
+++ src/main/java/pe/edu/upc/entities/Aula.java
@@ -2,6 +2,7 @@ package main.java.pe.edu.upc.entities;
 
 import main.java.pe.edu.upc.entities.enums.Materias;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Aula {
@@ -11,6 +12,9 @@ public class Aula {
 
     private Profesor profesor;
     private List<Estudiante> estudiantes;
+
+    private List<Estudiante> estudiantesPresentes;
+
 
     public Aula(int id, int maxEstudiantes, Materias materia, Profesor profesor,
                 List<Estudiante> estudiantes) {
@@ -68,16 +72,31 @@ public class Aula {
                 ", maxEstudiantes=" + maxEstudiantes +
                 ", materia=" + materia +
                 ", profesor=" + profesor +
-                ", estudiante=" + estudiantes +
+                ", estudiantes=" + estudiantes +
                 '}';
     }
 
     public boolean seDaraClases(){
         if(!this.profesor.Falto()){
             if(this.profesor.getMateria() == this.materia){
-                return this.estudiantes.size() >= (this.maxEstudiantes / 2);
+                for(Estudiante estudiante:this.estudiantes){
+                    if(!estudiante.Falto()){
+                        estudiantesPresentes.add(estudiante);
+                    }
+                }
+                return estudiantesPresentes.size() >= (this.maxEstudiantes / 2);
             }
         }
         return false;
+    }
+
+    public List<String> aprobados(){
+        List<String> alumnosAprobados = new ArrayList<>();
+        for(Estudiante estudiante: estudiantesPresentes){
+            if(estudiante.getNota() > 5){
+                alumnosAprobados.add(estudiante.getName());
+            }
+        }
+        return alumnosAprobados;
     }
 }
