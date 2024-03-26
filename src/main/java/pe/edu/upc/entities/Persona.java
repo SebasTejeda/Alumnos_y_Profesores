1043c9e4e4b006d0bed4b4b637fb7a258c9fc39d
Sebastian Tejeda <tejedasebastian129@gmail.com>
2024-03-26 10:47:57 -0500

    Se creó la clase Persona

--- src/main/java/pe/edu/upc/entities/Persona.java
+++ src/main/java/pe/edu/upc/entities/Persona.java
@@ -1,2 +1,46 @@
-package main.java.pe.edu.upc.entities;public class Persona {
+package main.java.pe.edu.upc.entities;
+
+public class Persona {
+    private String name;
+    private int edad;
+    private String sexo;
+
+    public Persona(String name, int edad, String sexo) {
+        this.name = name;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "name='" + name + '\'' +
+                ", edad=" + edad +
+                ", sexo='" + sexo + '\'' +
+                '}';
+    }
 }
